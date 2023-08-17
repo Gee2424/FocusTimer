e8cd40428be26449f0fcdcddd2fcc6012aa07a9c
Gee2424 <88376078+Gee2424@users.noreply.github.com>
2023-08-17 23:55:07 +0300

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,6 +1,4 @@
-# stats.py
 import json
-
 
 class Stats:
     def __init__(self, stats_file="stats.json"):
@@ -13,23 +11,33 @@ class Stats:
             with open(self.stats_file, "r") as f:
                 self.stats = json.load(f)
         except FileNotFoundError:
-            self.save_stats()  # Create a new stats file if it doesn't exist
+            self.save_stats()
 
     def save_stats(self):
         with open(self.stats_file, "w") as f:
             json.dump(self.stats, f)
 
+    def increment_counter(self, counter_name):
+        if counter_name in self.stats:
+            self.stats[counter_name] += 1
+            self.save_stats()
+
     def increment_work_sessions(self):
-        self.stats["work_sessions"] += 1
-        self.save_stats()
+        self.increment_counter("work_sessions")
 
     def increment_short_breaks(self):
-        self.stats["short_breaks"] += 1
-        self.save_stats()
+        self.increment_counter("short_breaks")
 
     def increment_long_breaks(self):
-        self.stats["long_breaks"] += 1
-        self.save_stats()
+        self.increment_counter("long_breaks")
 
     def get_stats(self):
         return self.stats
+
+# Usage example
+if __name__ == "__main__":
+    stats = Stats()
+    stats.increment_work_sessions()
+    stats.increment_short_breaks()
+    stats.increment_long_breaks()
+    print(stats.get_stats())
